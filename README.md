fa6a929150145484191239d6cf9e1b8622c71c08
Raidel Hernandez <raidel123@hotmail.com>
2018-04-19 04:33:49 -0400

    Edit gitignore for dump file add pip installation requirements Edit README and add download, installation, and requirements instructions Python script added and data being obtained from mongos database file.

--- README.md
+++ README.md
@@ -3,5 +3,37 @@
 Study based on [ Sentiment analysis of commit comments in GitHub: an empirical study ](https://dl.acm.org/citation.cfm?id=2597118)
 
 ## Requirements
+[ Python 2.x ] https://www.python.org/downloads/release/python-2714/
+[ Virtualenv ](https://virtualenv.pypa.io/en/stable/installation/)
+[ MongoDB ](https://pypi.org/project/pymongo/)
 
 ## Installation
+#### Setting up the virtual environment
+Creating and Activating
+```
+    $ virtualenv venv
+    $ source venv/bin/activate
+```
+Deactivating (after executing program)
+```
+    $ deactivate
+```
+#### Installing Requirements
+(after activating virtual environment)
+```
+    $ pip install -r requirements.txt
+```
+
+## Database Setup/Download
+[ MSR 2014 Mining Challenge Dataset ](http://ghtorrent.org/msr14.html)
+[ MongoDB database dump ](http://ghtorrent-downloads.ewi.tudelft.nl/datasets/msr14-mysql.gz)
+(perform these operations within the main repo directory, it should create a dump directory)
+```
+    $ wget http://ghtorrent-downloads.ewi.tudelft.nl/datasets/msr14-mongo.tar.gz
+    $ tar zxvf msr14-mongo.tar.gz
+```
+
+## Run
+```
+    $ python bsonparser.py
+```
---
+++ bsonparser.py
@@ -0,0 +1,33 @@
+import os
+import bson
+# import sqlite3
+# import pandas as pd
+
+# from pymongo import MongoClient
+from bson.json_util import loads
+
+dump_location = os.getcwd() + '/dump/msr14/'
+bson_ext = '.bson'
+
+# collection names (parameter types) can be found here: http://ghtorrent.org/mongo.html
+def GetDBTable(collection_name):
+
+    # Open the database file containing collection name in parameter.
+    # How to download the dataset and where to put it can be found on
+    # the README.
+    with open(dump_location + collection_name + bson_ext,'rb') as f:
+        # read the db table file and decode using bson
+        # may need to read the file in chunks (check results)
+        commit_comments = bson.decode_all( f.read() )
+
+        print commit_comments[0], '\n'            # dictionary, print single comment
+        print commit_comments[0].keys(), '\n'     # dictionary, print single comment keys
+        print commit_comments[0].values(), '\n'   # dictionary, print single comment values
+
+        # return is a dictionary (schemas can be found in documentation url fields: http://ghtorrent.org/mongo.html)
+        return commit_comments
+
+if __name__ == "__main__":
+
+    # example run obtaining data for commit_comments collection(table) name
+    GetDBTable('commit_comments')
