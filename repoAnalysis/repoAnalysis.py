cf11567cd61fd3aeefd8f76ff83f09fc7e196816
Tyler Kelly <tkelly@Tylers-MacBook-Pro.local>
2018-04-20 14:10:01 -0400

    set up database framework to connect with Andrew's sqlite db

--- repoAnalysis/repoAnalysis.py
+++ repoAnalysis/repoAnalysis.py
@@ -23,6 +23,11 @@ Used to hide stdout of pylint command
 import sys, os
 
 """
+wrapper for database connection
+"""
+from DB import Database
+
+"""
 Store login credentials in separate file
 Obtain personal access code by following steps here: https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
 
@@ -69,43 +74,58 @@ def authenticate():
 """
 TODO: retrieve users from SQL database and create array of Users objects initializing them with data from db
 """
-def retrieve_users():
-    pass
+def retrieve_users(db):
+    user_list_str = db.get('Users', 'commentor_login, sentiment_score', 100)
+    users = []
+    for u in user_list_str:
+        users.append(Users(u[0], u[1]))
+    return users
+    
 
 """
 TODO: establish connection with Andrew's database
 """
-def connect_to_db():
-    pass
+def connect_to_db(f):
+    db = Database()
+    db.open(f)
+    return db
 
 """
-go through all top level python documents within python projects and run them through pylint
+determine code quality of file temp with pylint 
+params: temporaryfile
+ret: float
+"""
+def get_code_quality_py(temp):
+    # temporarily hide stdout, stderr b/c pylint's Run will print there no matter what
+    _stdout = sys.stdout
+    _stderr = sys.stderr
+    null = open(os.devnull, 'wb')
+    sys.stdout = sys.stderr = null
+
+    # run pylint on temp file
+    results = Run([temp.name], exit=False)
+
+    # restore stdout
+    sys.stdout = _stdout
+    sys.stderr = _stderr
+    return results.linter.stats['global_note']
+
+"""
+go through all top level documents within projects and run them through language appropriate linters
 then average score for that user
 """
-def determine_code_quality(user, git_user):
+def examine_user_files(user, git_user):
     for repo in git_user.get_repos():
         if repo.language == "Python":
             for fs in repo.get_dir_contents('/'):
                 if fs.name.endswith(".py"):
-                    with tempfile.NamedTemporaryFile() as temp:
+                     with tempfile.NamedTemporaryFile() as temp:
                         # write string of decoded file to a temp file so pylint can read from it
                         temp.write(fs.decoded_content)
                         temp.flush()
-
-                        # temporarily hide stdout b/c Run will print there no matter what
-                        _stdout = sys.stdout
-                        _stderr = sys.stderr
-                        null = open(os.devnull, 'wb')
-                        sys.stdout = sys.stderr = null
-
-                        # run pylint on temp file
-                        results = Run([temp.name], exit=False)
-
-                        # restore stdout
-                        sys.stdout = _stdout
-                        sys.stderr = _stderr
-                        user.add_quality_score(results.linter.stats['global_note'])
+                        user.add_quality_score(get_code_quality_py(temp))
                         temp.close()
+                   
 
     
 
@@ -113,9 +133,11 @@ if __name__ == '__main__':
     g = authenticate()
     
     #TODO: read userid from SQL database to be added by andrew
-    user = Users("raidel123", 0)
-    git_user = g.get_user(user.username)
-    
-    determine_code_quality(user, git_user)
-    print user.username, user.qualityAverage, user.qualityScore
+    db = connect_to_db('temp.sql')
+    users = retrieve_users(db)
+
+    for u in users:
+        git_user = g.get_user(u.username)
+        examine_user_files(u, git_user)
+        print u.username, u.qualityAverage, u.qualityScore
 
